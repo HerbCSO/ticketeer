39ea1df6326a3f06772a80154b703b0b27b38904
Carsten Dreesbach <carsten.dreesbach@opower.com>
2013-10-13 14:04:05 -0400

    Correcting bad root_path

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     if request.env['omniauth.auth'].extra.raw_info.email !~ /@opower.com\Z/
       flash[:error] = "Sorry, you're not allowed to sign into this application."
-      redirect_to root
+      redirect_to root_path
     else
       @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 
