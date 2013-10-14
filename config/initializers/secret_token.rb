69845ba024b151223bb1d61983f7fb8f7fba39cf
Carsten Dreesbach <carsten.dreesbach@opower.com>
2013-10-14 18:00:30 -0400

    Making secret a secret again

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Ticketeer::Application.config.secret_key_base = '28a6e2a1349702aefd674f8e2374da4d6d1db4ee291c42c7e4ce39300b51fb43a56de2cf967128a35ea2c7c5f010804b6abca11cdff43c47cdd84020871b62df'
+Ticketeer::Application.config.secret_key_base = ENV['TICKETEER_SECRET']
