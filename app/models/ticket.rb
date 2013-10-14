728302f38ca6539837ec0567e8b5994d9008fc7e
Carsten Dreesbach <carsten.dreesbach@opower.com>
2013-10-13 21:11:37 -0400

    Turning scopes into lambdas for asset precompile
    The asset precompile step on Heroku deploy seems to trigger scoep evaluation,
    which then causes the deploy to fail because the DB isn't available at this
    point. Turning them into lambdas instead in order to work around this.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :user
 
-  scope :recent, where('created_at >= ?', 3.day.ago)
-  scope :next_open, where(served_at: nil).order(:created_at).first
+  scope :recent, -> { where('created_at >= ?', 3.days.ago) }
+  scope :next_open, -> { where(served_at: nil).order(:created_at).first }
 
   def now_serving?
     # self.user.ticket.where(served_at: nil).try(id)
